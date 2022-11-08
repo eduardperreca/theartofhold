7fa4dcd8d37f03aaa99fc8f29ea97d8407bc24a8
eduardperreca <eduardo.perreca@studenti.unimi.it>
2022-11-08 12:54:15 +0100

    switched tweets to eng for better aiming trained AI

--- Visualizzazione Scientifica/emotionCalculation/emotionVisualization.py
+++ Visualizzazione Scientifica/emotionCalculation/emotionVisualization.py
@@ -1,8 +1,11 @@
 import text2emotion as te
 import pandas as pd
+import os
+from googletrans import Translator
 
+translator = Translator()
 # Read the data
-df = pd.read_csv('tweetsRenzi.csv')
+df = pd.read_csv(os.path.join(os.path.dirname(__file__), "../tweetsRenzi.csv"))
 
 # Initialize the list of emotions counters
 countTot = 0
@@ -14,14 +17,18 @@ countFear = 0
 
 # Iterate on dataset and caluculate the emotion in percentage
 for index, row in df.iterrows():
-    
-    if index == 5000:
+
+    if index == 50:
         break
 
     print(index, 'esimo tweet')
 
-    emotions = te.get_emotion(row['Text'])
-    # print(emotions)
+    # Translate the tweet in english and calculate the emotion
+    translated = translator.translate(row['Text'], dest='en')
+    emotions = te.get_emotion(translated.text)
+    print(emotions, 'in inglese')
+
+
     if emotions['Happy'] != 0 or emotions['Angry'] != 0 or emotions['Surprise'] != 0 or emotions['Sad'] != 0 or emotions['Fear'] != 0:
         countTot = countTot + 1
         countHappy = countHappy + emotions['Happy']
@@ -32,10 +39,10 @@ for index, row in df.iterrows():
     index = index + 1
 
 # Print the results
-print("su un totale di ", index, " tweet, ", countTot, " sono stati classificati come positivi o negativi")
+print("su un totale di ", index, " tweet, ", countTot,
+      " sono stati classificati come positivi o negativi")
 print("Happy: ", countHappy/countTot * 100, "%")
 print("Angry: ", countAngry/countTot * 100, "%")
 print("Surprise: ", countSurprise/countTot * 100, "%")
 print("Sad: ", countSad/countTot * 100, "%")
 print("Fear: ", countFear/countTot * 100, "%")
-
