3f1962d94365780085daed78c481d28c042936c3
eduardperreca <eduardo.perreca@studenti.unimi.it>
2022-11-22 22:49:16 +0100

    added survey chart

---
+++ Visualizzazione Scientifica/scripts/plotSurvey.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+
+# read csv
+dictReplies = {}
+
+df = pd.read_csv(os.path.join(os.path.dirname(__file__),
+                 "../dataset/sondaggio.csv"), sep=",", encoding="latin-1")
+
+df = df[["Ti senti rappresentato dalla Politica attuale?"]]
+for i in df["Ti senti rappresentato dalla Politica attuale?"]:
+    if i in dictReplies.keys():
+        dictReplies[i] += 1
+    else:
+        dictReplies[i] = 1
+
+y = np.array([dictReplies[i] for i in dictReplies.keys()])
+
+plt.pie(y, autopct='%1.1f%%')
+plt.title("Rappresentatività della politica attuale")
+plt.show()
