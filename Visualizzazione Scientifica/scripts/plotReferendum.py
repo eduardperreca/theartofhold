f483782b05540f1413dcdacbc357dcbbf0f7c2f9
eduardperreca <eduardo.perreca@studenti.unimi.it>
2022-11-22 22:37:54 +0100

    camera script added

--- Visualizzazione Scientifica/scripts/plotReferendum.py
+++ Visualizzazione Scientifica/scripts/plotReferendum.py
@@ -5,28 +5,12 @@ from scipy.interpolate import interp1d
 import numpy as np
 
 # read csv
-
 dictYears = {}
 totVotanti = 0
 totElettori = 0
 years = []
 
 # merge csv
-temp = os.listdir(os.path.join(os.path.dirname(__file__), "../dataset/referendum"))
-temp = temp[0]
-year = temp.split("-")[1].split(".")[0]
-year = int(year[:4])
-df = pd.read_csv(os.path.join(os.path.dirname(__file__), "../dataset/referendum", temp), sep=";", encoding="latin-1")
-df["year"] = year
-df = df[["ELETTORI", "VOTANTI", "year"]]
-dictYears[year] = {
-    "ELETTORI": df["ELETTORI"].sum(),
-    "VOTANTI": df["VOTANTI"].sum(),
-    "PERCENTUALE": (df["VOTANTI"].sum() / df["ELETTORI"].sum() *100).round(),
-    "ANNO": year
-}
-
-i = 1
 for i in os.listdir(os.path.join(os.path.dirname(__file__), "../dataset/referendum")):
     totVotanti = 0
     totElettori = 0
@@ -53,8 +37,9 @@ cubic_interpolation_model = interp1d(x, y, kind = "cubic")
 x_ = np.linspace(x.min(), x.max(), 300)
 y_ = cubic_interpolation_model(x_)
 
-plt.plot(x_, y_, color = "red")
+plt.plot(x_, y_, color = "b")
+plt.grid(True)
 plt.title("Percentuale affluneza referendum dal 1974 al 2022")
 plt.xlabel("Anno")
-plt.ylabel("Percentuale votanti")
-plt.show()
+plt.ylabel("Affluenza (%)")
+plt.savefig(os.path.join(os.path.dirname(__file__), "../image/affluenzaReferendum.png")) 
