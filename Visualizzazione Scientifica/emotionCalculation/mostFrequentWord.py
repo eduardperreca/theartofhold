c94e2c3f6db59717a88b3aadc5feb28fb8079b90
eduardperreca <eduardo.perreca@studenti.unimi.it>
2022-12-04 22:06:09 +0100

    cleanup repo

--- Visualizzazione Scientifica/emotionCalculation/mostFrequentWord.py
+++ Visualizzazione Scientifica/emotionCalculation/mostFrequentWord.py
@@ -6,20 +6,21 @@ import threading
 
 for i in os.listdir(os.path.join(os.path.dirname(__file__), "../tweetPolitic")):
     fields = ['Text']
-    df = pd.read_csv(os.path.join(os.path.dirname(__file__), "../tweetPolitic/" + i), usecols=fields)
+    df = pd.read_csv(os.path.join(os.path.dirname(__file__),
+                     "../tweetPolitic/" + i), usecols=fields)
 
-    # Create a df of word from the text column with len greater than 5
     text = df[df['Text'].str.len() > 8]
 
     wordcloud = WordCloud(
         width=3000,
         height=2000,
         background_color='white'
-        ).generate(str(text))
+    ).generate(str(text))
 
     plt.imshow(wordcloud)
     plt.axis("off")
     i = i.split(".")[0]
     print("Saving " + i + " wordcloud")
     plt.show()
-    plt.savefig(os.path.join(os.path.dirname(__file__), f"../image/cameraRandom{i}.png"))
+    plt.savefig(os.path.join(os.path.dirname(__file__),
+                f"../image/cameraRandom{i}.png"))
