e84a434ed7060c5bc12b325c2ccf88f80eba2cd2
eduardperreca <eduardo.perreca@studenti.unimi.it>
2022-12-04 22:03:23 +0100

    cleaned wordcloud still doesn't make sense

--- Visualizzazione Scientifica/emotionCalculation/mostFrequentWord.py
+++ Visualizzazione Scientifica/emotionCalculation/mostFrequentWord.py
@@ -8,24 +8,18 @@ for i in os.listdir(os.path.join(os.path.dirname(__file__), "../tweetPolitic")):
     fields = ['Text']
     df = pd.read_csv(os.path.join(os.path.dirname(__file__), "../tweetPolitic/" + i), usecols=fields)
 
-    text = df['Text'].values
+    # Create a df of word from the text column with len greater than 5
+    text = df[df['Text'].str.len() > 8]
 
-    wordcloud = WordCloud().generate(str(text))
+    wordcloud = WordCloud(
+        width=3000,
+        height=2000,
+        background_color='white'
+        ).generate(str(text))
 
     plt.imshow(wordcloud)
     plt.axis("off")
+    i = i.split(".")[0]
+    print("Saving " + i + " wordcloud")
     plt.show()
     plt.savefig(os.path.join(os.path.dirname(__file__), f"../image/cameraRandom{i}.png"))
-
-# listName = []
-# for i in os.listdir(os.path.join(os.path.dirname(__file__), "../tweetPolitic")):
-#     listName.append(i)
-
-# listDirectory = []
-# for i in listName:
-#     print(i)
-#     listDirectory.append(os.path.join(os.path.dirname(__file__), f"../tweetPolitic/{i}"))
-    
-# for i in listDirectory:
-#     t = threading.Thread(target=mostFrequentWord, args=(i,))
-#     t.start()
