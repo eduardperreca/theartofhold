47ade50125a00d73765188544538e290dd0aba08
eduardperreca <eduardo.perreca@studenti.unimi.it>
2022-12-06 00:05:49 +0100

    nlkt freqWord

--- Visualizzazione Scientifica/emotionCalculation/mostFrequentWord.py
+++ Visualizzazione Scientifica/emotionCalculation/mostFrequentWord.py
@@ -2,20 +2,19 @@ import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 import pandas as pd
 import os
-import threading
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
 
 for i in os.listdir(os.path.join(os.path.dirname(__file__), "../tweetPolitic")):
     fields = ['Text']
     df = pd.read_csv(os.path.join(os.path.dirname(__file__),
-                     "../tweetPolitic/" + i), usecols=fields)
+                                "../tweetPolitic/" + i), usecols=fields)
 
-    for x in df["Text"]:
-        for j in x.split(" "):
-            if len(j) < 5:
-                df["Text"].replace(x, x.replace(j, ""), inplace=True)
-    
+    stop_words = set(stopwords.words('italian'))
+
+    df["Text"] = df["Text"].apply(lambda x: " ".join([word for word in word_tokenize(x) if word not in stop_words]))
+
     text = df["Text"]
-
     wordcloud = WordCloud(
         width=1800,
         height=3000,
@@ -24,8 +23,7 @@ for i in os.listdir(os.path.join(os.path.dirname(__file__), "../tweetPolitic")):
 
     plt.imshow(wordcloud)
     plt.axis("off")
-    i = i.split(".")[0]
-    print("Saving " + i + " wordcloud")
+    print(f"Saving {i} wordcloud")
     plt.show()
     plt.savefig(os.path.join(os.path.dirname(__file__),
-                f"../image/cameraRandom{i}.png"))
+                f"../image/camera{i}Random.png"))
