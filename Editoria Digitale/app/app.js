0434ce1b3606186cf33f841d2a20a8d2b656caf4
eduardperreca <eduardo.perreca@studenti.unimi.it>
2023-01-22 11:01:09 +0100

    idk

--- Editoria Digitale/app/app.js
+++ Editoria Digitale/app/app.js
@@ -1,6 +1,12 @@
 const express = require("express")
 const app = express()
 const PORT = 8080 || process.env.PORT
+const dotenv = require("dotenv")
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcrypt")
+const { default: mongoose } = require('mongoose')
+
+dotenv.config({ path: './.env' })
 
 app.set('view engine', 'hbs')
 
@@ -8,7 +14,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.get("/", (req, res) => {
-    res.send("Hello World")
+    res.render("login")
 })
 
 app.listen(PORT, () => {
